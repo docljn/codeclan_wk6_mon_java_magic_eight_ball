164586790aa14b4470f9bafce45a3bff64b17088
DocLJN <ljnoble@gmail.com>
2018-01-08 23:50:24 +0000

    Attempting to initialize by passing in answer argument.  Failed.

--- src/main/java/EightBall.java
+++ src/main/java/EightBall.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class EightBall {
 
     private ArrayList<String> answers;
 
 
-    public EightBall(){
-        this.answers = new ArrayList<>();
+    public EightBall(List<String> answer_list){
+
+        this.answers = new ArrayList<String>(Arrays.asList(answer_list));
+//        this.answers = new ArrayList<>();
+//        this.answers.addAll(answers);
         answers.add("Perhaps...");
         answers.add("It could be.");
         answers.add("Most decidedly so!");
